2ae3f41d571edf5c1bc5d2e24e3a30e1ac27f781
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2018-06-30 15:40:17 +0400

    pep8 and minor changes

--- DiskSpace_Analyser.py
+++ DiskSpace_Analyser.py
@@ -1,31 +1,38 @@
+'''
+calculates space occupied by files
+'''
+# std lib
 import os
 import shutil
-import matplotlib.pyplot as plt; plt.rcdefaults()
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 import time
+
+# 3rd party
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import matplotlib.pyplot as plt
 
 def calculate_size(start_path):
     total_size = 0
 
-    for dirpath, dirnames, filenames in os.walk(start_path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
-    if total_size>0:
+    for folder_path, folder_names, file_names in os.walk(start_path):
+        for file in file_names:
+            file_path = os.path.join(folder_path, file)
+            total_size += os.path.getsize(file_path)
+
+    if total_size > 0:
         return total_size
     else:
         return os.path.getsize(start_path)
 
 def get_filename_and_size(link):
     for files in os.listdir(link):
-        path=link+'\\'+files
-        size=calculate_size(path)
+        path = link+'\\'+files
+        size = calculate_size(path)
         arr.append(size/(1024*1024))
 
 def calculate_percentage(link):
-    total=sum(arr)
+    total = sum(arr)
     for i in arr:
         perc.append((i/total)*100)
 
@@ -33,33 +40,33 @@ def display_stats(link):
     get_filename_and_size(link)
     calculate_percentage(link)
     labels = os.listdir(link)
-    print('Total space occupied by directory(in MB) :',sum(arr))
-    df=pd.DataFrame({'Perc(%)':perc,'Size(in MB) ':arr,'File':labels})
-    df1=df.sort_values(by=['Perc(%)'],ascending=False)
+    print('Total space occupied by directory(in MB) :', sum(arr))
+    df = pd.DataFrame({'Perc(%)':perc, 'Size(in MB) ':arr, 'File':labels})
+    df1 = df.sort_values(by=['Perc(%)'], ascending=False)
     time.sleep(1)
     print(df1)
-    
-    for i in range(0,len(arr)-1):
-        for j in range (i+1,len(arr)):
-            if arr[i]>arr[j]:
-                temp=arr[i]
-                arr[i]=arr[j]
-                arr[j]=temp
-                temp1=labels[i]
-                labels[i]=labels[j]
-                labels[j]=temp1
-                
+
+    for i in range(0, len(arr)-1):
+        for j in range(i+1, len(arr)):
+            if arr[i] > arr[j]:
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+                temp1 = labels[i]
+                labels[i] = labels[j]
+                labels[j] = temp1
+
     #print(labels[len(labels)-5:len(labels)])
-    if len(os.listdir(link))>10:
-        header=labels[len(labels)-10:len(labels)]
-        val=arr[len(arr)-10:len(arr)]
+    if len(os.listdir(link)) > 10:
+        header = labels[len(labels)-10:len(labels)]
+        val = arr[len(arr)-10:len(arr)]
     else:
-        header=labels[0:len(labels)]
-        val=arr[0:len(arr)]
+        header = labels[0:len(labels)]
+        val = arr[0:len(arr)]
         
     y_pos = np.arange(len(header))
-    plt.barh(y_pos,val, align='center', alpha=0.6,color='red')
-    plt.yticks(y_pos,header)
+    plt.barh(y_pos, val, align='center', alpha=0.6, color='red')
+    plt.yticks(y_pos, header)
     
     plt.xlabel('Disk space used in MB')
     plt.ylabel('File Name')
@@ -67,63 +74,67 @@ def display_stats(link):
     time.sleep(1)
     plt.show()
 
-dirpath=input('enter the path of directory')
-dirpath.replace(':\\',':\\\\')
+print('enter the path of directory')
+dirpath = input('>>> ')
+dirpath.replace('\\', '/')
 if os.path.exists(dirpath) is False:
     print('incorrect directory path')
 else:
-    path=dirpath
-    arr=[]
-    perc=[]
-    if os.listdir(path)==[]:
+    path = dirpath
+    arr = []
+    perc = []
+    if os.listdir(path) == []:
         print('Empty directory')
     else:
         display_stats(path)
-        for i in range(1,1000):
-            print('Press 1 to go to child directory')
-            print('Press 2 to go to parent directory')
-            print('Press 3 to delete a subdirectory within current directory')
-            print('Press 4 to delete a file within current directory')
-            print('Press 5 to exit')
-            time.sleep(2)
-            op=input('enter your choice:')
-            if op=='1':
-                ch=input('enter the folder name:')
+        for i in range(1, 1000):
+            print('''
+Press 1 to go to child directory
+Press 2 to go to parent directory
+Press 3 to delete a subdirectory within current directory
+Press 4 to delete a file within current directory
+Press 5 to exit
+            ''')
+            # time.sleep(2)
+            print('enter your choice')
+            op = input('>>> ')
+            if op == '1':
+                ch = input('enter the folder name:')
 
                 if os.path.exists(path+'\\'+ch) is False:
                     print('Incorrect folder name')
                 else:
-                    path=path+'\\'+ch
-                    arr=[]
-                    perc=[]
-                    if os.listdir(path)==[]:
+                    path = path+'\\'+ch
+                    arr = []
+                    perc = []
+                    if os.listdir(path) == []:
                         print('Empty directory')
                     else:
                         display_stats(path)
-            elif op=='2':
-                path=path[:path.rfind('\\')]
-                arr=[]
-                perc=[]
+            elif op == '2':
+                path = path[:path.rfind('\\')]
+                arr = []
+                perc = []
                 display_stats(path)
-            elif op=='3':
-                subdir_name=input('enter the name of subdirectory to be deleted:')
-                if os.path.exists(path+'\\'+subdir_name) is False:
+            elif op == '3':
+                subdir_name = input('enter the name of subdirectory to be deleted:')
+                if os.path.exists(path+'/'+subdir_name) is False:
                     print('incorrect subdirectory name')
                 else:
-                    shutil.rmtree(path+'\\'+subdir_name)
-                    arr=[]
-                    perc=[]
+                    shutil.rmtree(path+'/'+subdir_name)
+                    arr = []
+                    perc = []
                     display_stats(path)
-            elif op=='4':
-                file_name=input('enter the name of file to be deleted:')
+            elif op == '4':
+                file_name = input('enter the name of file to be deleted:')
                 if os.path.exists(path+'\\'+file_name) is False:
                     print('incorrect file name')
                 else:
                     os.remove(path+'\\'+file_name)
-                    arr=[]
-                    perc=[]
+                    arr = []
+                    perc = []
                     display_stats(path)
-            elif op=='5':
+            elif op == '5':
                 print('Thank you')
                 break
             else:
